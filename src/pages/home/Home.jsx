87954746dc5b06c0cd741d438b35bf5efae33223
Yassine <baadiyassine01@gmail.com>
2025-06-05 11:44:06 +0200

    filter button

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,29 +1,46 @@
 import './home.css'
+import { useState } from 'react';
 
-export default function Home(){
+export default function Home() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedRegion, setSelectedRegion] = useState("Filter by Region");
 
+    const regions = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
-    return(
+    const handleSelect = (region) => {
+        setSelectedRegion(region);
+        setIsOpen(false);
+        
+    };
 
-
+    return (
         <>
-        <section className='homeContainer'>
+            <section className='homeContainer'>
+                <div className='searchAndFilter'>
+                    <div className='searchContainer'>
+                        <span>&#128269;</span>
+                        <input type="search" placeholder='Search for a country...' />
+                    </div>
 
-            <div className='searchAndFilter'>
-                <div className='searchContainer'>
-                    <span>&#128269;</span>
-                    <input type="search" placeholder='Search for a country...' />
+                    <div className='filterContainer'>
+                        <div className='dropdown'>
+                            <button onClick={() => setIsOpen(!isOpen)} className='dropdown-button'>
+                                {selectedRegion}
+                                <span className='arrow'>▾</span>
+                            </button>
+                            {isOpen && (
+                                <ul className='dropdown-menu' >
+                                {regions.map((element) => (
+                                    <li key={element} className='dropdown-item' onClick={()=>{handleSelect}}>
+                                        {element}
+                                    </li>
+                                ))}
+                                </ul>
+                            )}
+                        </div>
+                    </div>
                 </div>
-                <div className='filterContainer'>
-                
-                </div>
-            </div>
-
-            
-
-        </section>
-        
-        
+            </section>
         </>
     )
 }
