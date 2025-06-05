c95a6a1415bb326b6ad273db9c6f5f3bfd71132f
GoNEAR <cagatay.morgul@outlook.com>
2025-06-05 13:42:28 +0200

    details contenue dynbamique

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,60 +1,92 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Details.css";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
-export default function Details({ data }) {
+export default function Details({ allCountries }) {
   const navigate = useNavigate();
+  const { countryCode } = useParams();
+  
+  const dataCountry = allCountries ? allCountries.find((c) => c.cca3 === countryCode) : null;
+
   return (
     <>
-      <section className="sectionDetails">
-        <button onClick={() => navigate(-1)} className="detailsBackBtn">
-          <FontAwesomeIcon icon={faArrowLeft} />
-          &nbsp;&nbsp;&nbsp;Back
-        </button>
-        <div className="details">
-          <div className="detailsImg">
-            <img src={data?.flags.png} alt="" />
+      {dataCountry && allCountries ? (
+        <section className="sectionDetails">
+          <button onClick={() => navigate(-1)} className="detailsBackBtn">
+            <FontAwesomeIcon icon={faArrowLeft} />
+            &nbsp;&nbsp;&nbsp;Back
+          </button>
+          <div className="details">
+            <div className="detailsImg">
+              <img src={dataCountry.flags.png} alt="" />
+            </div>
+            <div className="detailsTexts">
+              <div className="detailsTitle">
+                <h2>{dataCountry.name.common}</h2>
+              </div>
+              <div className="detailsDescription">
+                <p>
+                  <span>Native Name: </span>
+                  {dataCountry.name.nativeName ? dataCountry.name.nativeName[Object.keys(dataCountry.name.nativeName)[0]].common : "No native name"}
+                </p>
+                <p>
+                  <span>Population: </span>
+                  {dataCountry.population ? dataCountry.population.toLocaleString("en-EN") : "No dataCountry"}
+                </p>
+                <p>
+                  <span>Region: </span>
+                  {dataCountry.region ? dataCountry.region : "No region"}
+                </p>
+                <p>
+                  <span>Sub Region: </span>
+                  {dataCountry.subregion ? dataCountry.subregion : "No subregion"}
+                </p>
+                <p>
+                  <span>Capital: </span>
+                  {dataCountry.capital ? dataCountry.capital.join(", ") : "No capital"}
+                </p>
+                <p>
+                  <span>Top Level Domain: </span>
+                  {dataCountry.tld ? dataCountry.tld.join(", ") : "No TLD"}
+                </p>
+                <p>
+                  <span>Currencies: </span>
+                  {dataCountry.currencies
+                    ? Object.values(dataCountry.currencies)
+                        .map((cur) => cur.name)
+                        .join(", ")
+                    : "No currencies"}
+                </p>
+                <p>
+                  <span>Languages: </span>
+                  {dataCountry.languages ? Object.values(dataCountry.languages).join(", ") : "No languages"}
+                </p>
+              </div>
+              <div className="detailsBorderCountries">
+                <p>Border Countries: </p>
+                {dataCountry.borders
+                  ? dataCountry.borders.map((border, i) => {
+                      const country = allCountries.find((c) => c.cca3 === border);
+                      let nativeName = border;
+                      if (country && country.name && country.name.nativeName) {
+                        const nativeKeys = Object.keys(country.name.nativeName);
+                        if (nativeKeys.length > 0) {
+                          nativeName = country.name.nativeName[nativeKeys[0]].common;
+                        } else if (country.name.common) {
+                          nativeName = country.name.common;
+                        }
+                      }
+                      return <Link key={i}>{nativeName}</Link>;
+                    })
+                  : "No borders"}
+              </div>
+            </div>
           </div>
-          <div className="detailsTexts">
-            <div className="detailsTitle">
-              <h2>{data?.name.common}</h2>
-            </div>
-            <div className="detailsDescription">
-              <p>
-                <span>Native Name: {}</span>
-              </p>
-              <p>
-                <span>Population: {}</span>
-              </p>
-              <p>
-                <span>Region: {}</span>
-              </p>
-              <p>
-                <span>Sub Region: {}</span>
-              </p>
-              <p>
-                <span>Capital: {}</span>
-              </p>
-              <p>
-                <span>Top Level Domain: {}</span>
-              </p>
-              <p>
-                <span>Currencies: {}</span>
-              </p>
-              <p>
-                <span>Languages: {}</span>
-              </p>
-            </div>
-            <div className="detailsBorderCountries">
-              <p>Border Countries: </p>
-              {data?.borders.map((border, i) => (
-                <Link key={i}>{border}</Link>
-              ))}
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
+      ) : (
+        <p>Loading</p>
+      )}
     </>
   );
 }
